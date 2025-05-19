f56e8a4cbdb8a4d6e33562b7928e28c7519b81d5
arvasil <133312794+arvasil@users.noreply.github.com>
2025-05-20 02:59:35 +0530

    base project added

---
+++ src/main/java/com/strut/crictribe/CricTribeApplication.java
@@ -0,0 +1,13 @@
+package com.strut.crictribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricTribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricTribeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strut/crictribe/CricTribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strut.crictribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricTribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
